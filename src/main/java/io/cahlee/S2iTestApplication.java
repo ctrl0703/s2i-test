8d1410a23b73659313bfe38764efed231a12bcf5
cahlee <cahlee@kr.ibm.com>
2020-04-09 14:54:54 +0900

    first commit

---
+++ src/main/java/io/cahlee/S2iTestApplication.java
@@ -0,0 +1,13 @@
+package io.cahlee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S2iTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S2iTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/cahlee/S2iTestApplicationTests.java
@@ -0,0 +1,13 @@
+package io.cahlee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S2iTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
